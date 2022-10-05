e5a6adf1dd3c0357e04f39064a9cde12cdcee775
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-04 19:14:46 -0500

    Clases en Clases/

---
+++ Clases/Carro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases.Clases {
+    public class Carro {
+        public string _marca;
+        public int _anio;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Clases.Clases;
+
 Console.WriteLine("Hello, World!");
+
+Persona ConstruirPersona(string nombre, string apellido, DateTime fechaNac) {
+    var persona = new Persona();
+    persona._nombre = nombre;
+    persona._apellido = apellido;
+    persona._fechaNacimiento = fechaNac;
+
+    return persona;
+}
+
+Console.WriteLine(ConstruirPersona("Fer", "Santi", new DateTime(1996, 12, 16)));
